351c2016a395aba6d5a7fe9cd06c47a31efc1e5a
Rubens Brock <r.brocksilva@yahoo.com.br>
2021-05-21 09:09:44 -0300

    Styles and font configs - OK

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import styles from '../styles/home.module.scss';
+import Head from 'next/head';
+
 export default function Home() {
   return (
-    <h1>Hello, world!</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+      <h1>
+        Hello, <span>world</span>!
+      </h1>
+    </>
   )
 }
