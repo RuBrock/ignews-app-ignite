738e5bdcee0e89a0fa09343d3dbb311aa0cf8163
Rubens Brock <r.brocksilva@yahoo.com.br>
2021-05-23 19:00:44 -0300

    Home page OK, Subscribe button OK

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import styles from '../styles/home.module.scss';
+import styles from './home.module.scss';
 import Head from 'next/head';
+import { SubscribeButton } from '../components/SubscribeButton';
 
 export default function Home() {
   return (
@@ -7,9 +8,22 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1>
-        Hello, <span>world</span>!
-      </h1>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+
+          <p>
+          Get acess to all the publications <br /> 
+          <span>for $9.90 month</span>
+          </p>
+
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
